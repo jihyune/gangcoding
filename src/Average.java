000a7b4dc6933e743de96df40b7660f2901d6b3f
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-04-29 15:33:46 +0900

    initialize jw

---
+++ src/Average.java
@@ -0,0 +1,9 @@
+public class Average {
+    public static void main(String[] args) {
+        int a = 80;
+        int b = 75;
+        int c = 55;
+        int avr = (a + b + c) / 3;
+        System.out.println(avr);
+    }
+}
