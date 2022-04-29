5c5b3d0fea1e6f6b4ac9327cef68b33712685f52
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-04-29 15:20:43 +0900

    initialize jw

---
+++ src/bj_bronze_4_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bj_bronze_4_1 {
+    public static void main (String[] args){
+        Scanner sc1 = new Scanner(System.in);
+        int a = sc1.nextInt();
+        int b = sc1.nextInt();
+        double result = (double) a / b;
+        System.out.println(result);
+    }
+}
